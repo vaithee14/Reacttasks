dfa10cc6d5cde915ffeaeae8913808fd9f19f077
vaithee14 <svaithi2004@gmail.com>
2025-01-22 15:49:37 +0530

    updates

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,13 +3,23 @@ import Navbar from "./navbar/Navbar";
 import SubmitButton from "./Submitbtn";
 
 export default function Home() {
+  // const name = "vaitheeshwari";
+
+  const userData = {
+    name: "vaitheeshwari",
+    mobile: 385738535,
+    address: "no:2,aaa street",
+    marks: 848,
+  };
+
   return (
     <>
       <Navbar />
-      
-      <h3>WELCOME</h3>
+
+      <h3>WELCOME {userData.name}</h3>
+      <p>your mobile number is: {userData.mobile}</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      <Card />
+      <Card data={userData} />
       <SubmitButton />
     </>
   );
